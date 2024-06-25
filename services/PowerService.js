4651beeb32ddb140c6ef7e3506b5f319a2d2bd2f
BN0b13 <NobleNotes1@gmail.com>
2024-06-25 13:37:55 -0700

    added back in raspberry pi gpio code

--- services/PowerService.js
+++ services/PowerService.js
@@ -1,16 +1,16 @@
 import { Gpio } from 'onoff';
 
-// const PowerOutlet = new Gpio(21, 'out');
+const PowerOutlet = new Gpio(21, 'out');
 
 class PowerService {
 
     async cycleDownstairsPumps(time) {
         console.log('Turning on pumps...');
-        // PowerOutlet.writeSync(1);
+        PowerOutlet.writeSync(1);
 
         setTimeout(() => {
             console.log('Shutting down pumps...');
-            // PowerOutlet.writeSync(0);
+            PowerOutlet.writeSync(0);
         }, time);
 
         return {
